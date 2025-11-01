f0043544a174e198332aabae47e118131ac9c96f
matuszsadawa-code <matuszsadawa@gmail.com>
2025-11-01 10:54:13 +0100

    Remove unused import of React in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import PersonalIntro from './components/sections/PersonalIntro'
 import SocialHub from './components/sections/SocialHub'
